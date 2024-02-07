ca221bb44405bd1c0281da3d26e2aa8277995aae
aminu <talk2aminulawal@gmail.com>
2024-02-07 15:30:31 +0100

    updated stream_df to output all columns in console

--- src/jobs/streaming-spark.py
+++ src/jobs/streaming-spark.py
@@ -1,14 +1,41 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, FloatType, DataType
+from pyspark.sql.functions import from_json, col
+
 
 def start_spark_streaming(spark) -> None:
+    try:
+        stream_df = (
+            spark.readStream.format("socket")
+            .option("host", "127.0.0.1")
+            .option("port", 9999)
+            .load()
+        )
 
-    stream_df = spark.readStream.format('socket').option('host', 'localhost').option('port', 9999).load()
+        schema = StructType(
+            [
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("stars", FloatType()),
+                StructField("date", StringType()),
+                StructField("text", StringType()),
+            ]
+        )
 
-    query = stream_df.writeStream.outputMode('append').format('console').start()
+        stream_df = stream_df.select(
+            from_json(col("value"), schema).alias("data")
+        ).select("data.*")
 
-    query.awaitTermination()
+        query = stream_df.writeStream.outputMode("append").format("console").start()
 
-if __name__ == '__main__':
-    spark = SparkSession.builder.appName('SocketStreamConsumer').getOrCreate()
+        query.awaitTermination()
+
+    except Exception as e:
+        print("Error occured: %s", e)
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("SocketStreamConsumer").getOrCreate()
 
     start_spark_streaming(spark)
